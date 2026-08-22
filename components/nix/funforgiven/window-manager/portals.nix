763c916ee9b4bd6c92c543b46025dbaf1d8227cb
funforgiven <fahricanelidemir@gmail.com>
2026-08-22 18:49:14 +0300

    fix(portals): use KDE file chooser

--- components/nix/funforgiven/window-manager/portals.nix
+++ components/nix/funforgiven/window-manager/portals.nix
@@ -16,17 +16,19 @@ _: {
           enable = true;
           xdgOpenUsePortal = true;
           extraPortals = [
+            pkgs.kdePackages.xdg-desktop-portal-kde
             pkgs.xdg-desktop-portal-gtk
           ];
           config = {
             niri = {
               default = [
-                "gnome"
+                "kde"
                 "gtk"
               ];
-              "org.freedesktop.impl.portal.Access" = "gtk";
-              "org.freedesktop.impl.portal.FileChooser" = "gtk";
+              "org.freedesktop.impl.portal.FileChooser" = "kde";
               "org.freedesktop.impl.portal.Notification" = "gtk";
+              "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+              "org.freedesktop.impl.portal.Screenshot" = "gnome";
               "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
             };
           };
