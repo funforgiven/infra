d6ed21e967e2b85129a029b9f22d37b2afabb655
funforgiven <fahricanelidemir@gmail.com>
2026-08-23 00:15:19 +0300

    fix(aws): use managed key alias ARNs

--- components/cloud/services/mail-aws/tofu/storage.tf
+++ components/cloud/services/mail-aws/tofu/storage.tf
@@ -17,13 +17,21 @@ resource "aws_db_instance" "mail" {
   max_allocated_storage = 100
   storage_type          = "gp3"
   storage_encrypted     = true
-  kms_key_id            = "alias/aws/rds"
+  kms_key_id = join("", [
+    "arn:aws:kms:eu-central-1:",
+    data.aws_caller_identity.current.account_id,
+    ":alias/aws/rds",
+  ])
 
-  db_name                       = "stalwart"
-  username                      = "stalwart"
-  manage_master_user_password   = true
-  master_user_secret_kms_key_id = "alias/aws/secretsmanager"
-  port                          = 5432
+  db_name                     = "stalwart"
+  username                    = "stalwart"
+  manage_master_user_password = true
+  master_user_secret_kms_key_id = join("", [
+    "arn:aws:kms:eu-central-1:",
+    data.aws_caller_identity.current.account_id,
+    ":alias/aws/secretsmanager",
+  ])
+  port = 5432
 
   db_subnet_group_name   = aws_db_subnet_group.mail.name
   vpc_security_group_ids = [aws_security_group.database.id]
